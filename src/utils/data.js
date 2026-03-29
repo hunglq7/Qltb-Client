b27a751253692a8d64700c2a2664d96a98299932
Le Quang Hung <hunglq7@gmail.com>
2026-03-29 16:05:05 +0700

    16h05-29.03.2026

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,13 +1,13 @@
-import bomnuoc from '/src/assets/images/thietbi/Bomnuoc.jpg';
-import maycao from '/src/assets/images/thietbi/mangcao-sgb.jpg';
-import bangtai from '/src/assets/images/thietbi/Bang-tai.jpg';
-import bienap from '/src/assets/images/thietbi/Bienap.jpg';
-import capdien from '/src/assets/images/thietbi/capdien.png';
-import aptomat from '/src/assets/images/thietbi/Ap-to-mat.png';
-import maykhoan from '/src/assets/images/thietbi/Maykhoan.jpg';
-import khoidongtu from '/src/assets/images/thietbi/khoi-dong-tu.jpg';
-import toidien from '/src/assets/images/thietbi/toidien.jpg';
-import mayxuc from '/src/assets/images/thietbi/ZCY-60.jpg';
+import bomnuoc from '/assets/images/thietbi/Bomnuoc.jpg';
+import maycao from '/assets/images/thietbi/mangcao-sgb.jpg';
+import bangtai from '/assets/images/thietbi/Bang-tai.jpg';
+import bienap from '/assets/images/thietbi/Bienap.jpg';
+import capdien from '/assets/images/thietbi/capdien.png';
+import aptomat from '/assets/images/thietbi/Ap-to-mat.png';
+import maykhoan from '/assets/images/thietbi/Maykhoan.jpg';
+import khoidongtu from '/assets/images/thietbi/khoi-dong-tu.jpg';
+import toidien from '/assets/images/thietbi/toidien.jpg';
+import mayxuc from '/assets/images/thietbi/ZCY-60.jpg';
 
 export const myData = [
   {
